0bb1e9415def6c3abdcdae9bb6698e9b7ee96c4f
Vikas Joshi <vikasjoshi10889@gmail.com>
2026-04-02 21:39:30 +0530

    feat: add userRoutes and seperate socket from root

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,31 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 import { createServer } from "http";
 import { Server } from "socket.io";
 import mongoose from "mongoose";
 import cors from "cors";
+import { connectToSocket } from "./controllers/socketManager.js";
 
 const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", process.env.PORT || 8000);
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
 const start = async () => {
-  app.listen(8000, () => {
+  const connectDb = await mongoose.connect(process.env.MONGO_URI);
+  if (connectDb) {
+    console.log("connected to database");
+  }
+  server.listen(app.get("port"), () => {
     console.log("server is running on port 8000");
   });
 };
