a97ee8d119cd298f67b6f647b23f4741d36df73f
Vikas Joshi <vikasjoshi10889@gmail.com>
2026-04-04 23:20:17 +0530

    feat: add user routes login and register

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,7 @@ import { Server } from "socket.io";
 import mongoose from "mongoose";
 import cors from "cors";
 import { connectToSocket } from "./controllers/socketManager.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 const server = createServer(app);
@@ -15,6 +16,7 @@ app.set("port", process.env.PORT || 8000);
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
+app.use("/api/v1/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("hello world");
---
+++ backend/src/controllers/user.js
@@ -0,0 +1,53 @@
+import { User } from "../models/user.js";
+import bcrypt, { hash } from "bcrypt";
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ message: "Please provide username and password" });
+  }
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    if (bcrypt.compare(password, user.password)) {
+      let token = crypto.randomBytes(20).toString("hex");
+      user.token = token;
+      await user.save();
+      return res
+        .status(200)
+        .json({ message: "logged in successfully" }, { token: token });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: `Something went wrong ${err}` });
+  }
+};
+
+const register = async (req, res) => {
+  const { name, username, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res
+        .status(httpStatus.FOUND)
+        .json({ message: "User already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      name: name,
+      username: username,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    res.status(httpStatus.CREATED).json({ message: "User registered" });
+  } catch (err) {
+    res.json({ message: `Something went wrong ${err}` });
+  }
+};
+
+export { login, register };
