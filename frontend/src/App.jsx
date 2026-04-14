9cfd10bb591ce6e347b48d5ce963a57b6d225318
Vikas Joshi <vikasjoshi10889@gmail.com>
2026-04-14 20:39:33 +0530

    feat: setup the router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
 import "./App.css";
 import LandingPage from "./pages/landing";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <LandingPage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </Router>
     </>
   );
 }
