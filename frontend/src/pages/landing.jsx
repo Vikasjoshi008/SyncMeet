101282947498ed6f606acfe8e2ae1c19e1d78dcb
Vikas Joshi <vikasjoshi10889@gmail.com>
2026-04-15 23:13:35 +0530

    feat: add better UI and creating landingPage

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import "../App.css";
 
 export default function LandingPage() {
   return (
     <>
-      <h1>Landing page</h1>
+      <div className="landingPageContainer">
+        <h1>Landing page</h1>
+      </div>
     </>
   );
 }
